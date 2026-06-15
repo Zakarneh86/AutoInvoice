2294cda4a654c84acfcbbd61b4765a1aaf474cf5
Zakarneh86 <98852242+Zakarneh86@users.noreply.github.com>
2026-06-15 20:42:40 +0000

    Streamlit Page Setup

---
+++ streamlit.py
@@ -0,0 +1,48 @@
+import streamlit as st
+
+
+PO_OPTIONS = [
+    "Select PO",
+    "PO 001",
+    "PO 002",
+    "PO 003",
+]
+
+ROLE_OPTIONS = [
+    "Select role",
+    "Project Manager",
+    "Business Analyst",
+    "Developer",
+    "QA Engineer",
+]
+
+LOCATION_OPTIONS = [
+    "Select location",
+    "Onsite",
+    "Offshore",
+    "Remote",
+]
+
+
+st.set_page_config(
+    page_title="Auto Invoice",
+    page_icon=":page_facing_up:",
+    layout="centered",
+)
+
+st.title("Auto Invoice")
+
+po = st.selectbox("PO Droplist", PO_OPTIONS)
+role = st.selectbox("Role Droplist", ROLE_OPTIONS)
+location = st.selectbox("Location Droplist", LOCATION_OPTIONS)
+
+timesheets = st.file_uploader(
+    "Timesheets upload area",
+    type=["pdf", "xlsx", "xls", "csv"],
+    accept_multiple_files=True,
+)
+
+if timesheets:
+    st.subheader("Uploaded timesheets")
+    for timesheet in timesheets:
+        st.write(timesheet.name)
